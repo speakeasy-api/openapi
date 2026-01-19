361dde0b79de28f507cdfd774fc8371f4d6c7eae
Tristan Cartledge <108070248+TristanSpeakEasy@users.noreply.github.com>
2026-01-19 11:43:29 +1000

    feat(cli): add overlay upgrade command for upgrading to Overlay 1.1.0 (#131)

--- cmd/openapi/commands/overlay/root.go
+++ cmd/openapi/commands/overlay/root.go
@@ -5,5 +5,6 @@ import "github.com/spf13/cobra"
 func Apply(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(applyCmd)
 	rootCmd.AddCommand(compareCmd)
+	rootCmd.AddCommand(upgradeCmd)
 	rootCmd.AddCommand(validateCmd)
 }
